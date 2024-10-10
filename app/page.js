663872d15ef3bc4b210e2263182f0a17bbb02e6c
chen4023 <ads4023@naver.com>
2024-10-10 16:23:56 +0900

    [feat] 메인페이지 기능 구현

--- app/page.js
+++ app/page.js
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <></>;
+import Card from "@/components/Card";
+
+export default async function Home() {
+  const res = await fetch("http://localhost:3000/api/animal");
+  const data = await res.json();
+  console.log(data);
+  return (
+    <ul>
+      {data.map((el) => (
+        <Card key={el.id} animal={el} />
+      ))}
+    </ul>
+  );
 }
