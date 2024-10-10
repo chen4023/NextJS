7b552e6c2b9e7a17c6bef25ce747fec000307930
chen4023 <ads4023@naver.com>
2024-10-10 16:28:16 +0900

    [feat] 상세페이지 기능 구현

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -1,3 +1,17 @@
-export default function Detail() {
-  return <div>Detail</div>;
+import FavoriteButton from "@/components/FavoriteButton";
+
+export default async function Detail({ params }) {
+  const animalId = params.id;
+  const res = await fetch(`http://localhost:3000/api/animal/${animalId}`);
+  const animalData = await res.json();
+  return (
+    <section className="detail">
+      <img src={animalData?.img.src} />
+      <h2>
+        {animalData?.name}
+        <FavoriteButton animalId={animalData.id} />
+      </h2>
+      <div>{animalData?.description}</div>
+    </section>
+  );
 }
